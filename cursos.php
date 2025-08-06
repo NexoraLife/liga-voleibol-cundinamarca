0e5447fb1d28890e405cd3ce8dceb543571cd772
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:07:01 -0500

    Delete cursos.php

--- cursos.php
+++
@@ -1,6 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Cursos</h1>
-    <p>Capacitación, formación y academias para entrenadores y deportistas.</p>
-</main>
-<?php include 'footer.php'; ?>
