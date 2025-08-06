4410ba372c8b34e5c5fa9e1f364da35c73705d0a
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:07:39 -0500

    Delete jueces.php

--- jueces.php
+++
@@ -1,6 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Jueces</h1>
-    <p>Información para jueces, certificaciones y eventos relacionados.</p>
-</main>
-<?php include 'footer.php'; ?>
