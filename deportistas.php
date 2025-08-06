20e250039a8d59a80a6bb652f049c43d361d4763
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:07:08 -0500

    Delete deportistas.php

--- deportistas.php
+++
@@ -1,6 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Deportistas</h1>
-    <p>Información, convocatorias y desarrollo para deportistas.</p>
-</main>
-<?php include 'footer.php'; ?>
