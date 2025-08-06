0c8f7b22073639c75315bf02722454ad53d46609
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:07:47 -0500

    Delete nosotros.php

--- nosotros.php
+++
@@ -1,9 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Nosotros</h1>
-    <p>
-        <!-- Agrega aquí la información institucional de la Liga -->
-        Liga de Voleibol de Cundinamarca: trayectoria, misión, visión y valores.
-    </p>
-</main>
-<?php include 'footer.php'; ?>
