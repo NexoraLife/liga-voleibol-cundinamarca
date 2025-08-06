849a138104305201bca922f9d9dc38fd1211ad2b
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:07:16 -0500

    Delete entrenadores.php

--- entrenadores.php
+++
@@ -1,6 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Entrenadores</h1>
-    <p>Certificación, capacitación y recursos para entrenadores.</p>
-</main>
-<?php include 'footer.php'; ?>
