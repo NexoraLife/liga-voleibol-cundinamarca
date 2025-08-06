ebbc32038c6c84c333ffc85f702ef5b232ec4685
NexoraLife <nexoralife01@gmail.com>
2025-08-06 01:05:11 -0500

    Delete contacto.php

--- contacto.php
+++
@@ -1,7 +0,0 @@
-<?php include 'menu.php'; ?>
-<main class="container mx-auto px-6 py-16">
-    <h1 class="text-3xl font-bold mb-8">Contacto</h1>
-    <p>Puedes escribirnos al correo <a href="mailto:info@ligavoleibolcundinamarca.org">info@ligavoleibolcundinamarca.org</a></p>
-    <!-- Formulario de contacto aquí si lo deseas -->
-</main>
-<?php include 'footer.php'; ?>
